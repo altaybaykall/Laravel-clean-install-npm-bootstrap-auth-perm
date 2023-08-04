11c31d064d609386efa92fec9bcf90d96842f8bb
Altay Baykal <altaybaykall@gmail.com>
2023-08-04 15:24:31 +0300

    first

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Super',
+            'email' => 'superadmin@hotmail.com',
+            'password' => bcrypt('1234567'),
+        ]);
+        DB::table('model_has_roles')->insert([
+            'role_id' => 1,
+            'model_type' => 'App\Models\User',
+            'model_id' => 1
+        ]);
+    }
+}
